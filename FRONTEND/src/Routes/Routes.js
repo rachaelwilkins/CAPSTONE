af09fd34b24aa4c59c3e1805466b48ac75b39f65
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-07-17 18:54:40 +1200

    updated routes

--- FRONTEND/src/Routes/Routes.js
+++ FRONTEND/src/Routes/Routes.js
@@ -1,22 +1,26 @@
 import React, { } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import SneakerDetailsDetails from '../pages/GameDetails';
+import SneakerDetails from '../Pages/SneakerDetails';
+import MensSneakers from '../Pages/Men';
+import WomensSneakers from '../Pages/Women';
+import BoysSneakers from '../Pages/Boys'
+import GirlsSneakers from '../Pages/Girls'
+import Blog from '../Pages/Blog'
+import LogInPage from '../Pages/LoginSignUp';
 import CartDetails from '../pages/CartDetails';
-import SearchPage from '../pages/SearchPage';
-import LoginSignUp from '../Pages/LoginSignUp';
-import { Login } from '@mui/icons-material';
 
 export const AppRoutes = (props) => {
   return (
 
     <Routes>
-      <Route exact path='/sneakers/:id/' element={<SneakerDetails {...props} />} />
-      <Route exact path='/men' element={<Men {...props} />} />
-      <Route exact path="/women" element={<Women {...props} />} />
-      <Route exact path="/boys" element={<Boys {...props} />} />
-      <Route exact path="/girls" element={<Girls {...props} />} />
-      <Route exact path="/login" element={<Login {...props} />} />
-      <Route exact path='/cart' element={<CartDetails {...props} />} />
+      <Route exact path="/sneakers/:id/" element={<SneakerDetails {...props} />} />
+      <Route exact path="/men" element={<MensSneakers {...props} />} />
+      <Route exact path="/women" element={<WomensSneakers {...props} />} />
+      <Route exact path="/boys" element={<BoysSneakers {...props} />} />
+      <Route exact path="/girls" element={<GirlsSneakers {...props} />} />
+      <Route exact path="/blog" element={<Blog {...props} />} />
+      <Route exact path="/login" element={<LogInPage {...props} />} />
+      <Route exact path="/cart" element={<CartDetails {...props} />} />
     </Routes>
   )
 }
